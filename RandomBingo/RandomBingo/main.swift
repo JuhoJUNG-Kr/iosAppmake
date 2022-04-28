d689a3d089857aae151d57573f358190e42e459c
Juho Jung <jooho293@gmail.com>
2022-04-28 17:32:08 +0200

    Style: logic for bingo game
    -

--- RandomBingo/RandomBingo/main.swift
+++ RandomBingo/RandomBingo/main.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+var computerChoice = Int.random(in: 1...100)
+var myChoice = 0
+
+while true {
+    var userInput = readLine()
+    
+    if let input = userInput {
+        if let number = Int(input) {
+            myChoice = number
+        }
+    }
+    
+    
+    if computerChoice > myChoice {
+        print("UP!")
+    } else if computerChoice < myChoice {
+        print("DOWN!")
+    } else {
+        print("Bingo!")
+        break
+    }
+}
