78db891dfd21a397293d2e9df5ed885fe912b5b2
Juho Jung <jooho293@gmail.com>
2022-07-06 18:57:19 +0200

    Feat: timer
    -

--- PracticeBasicApp/PracticeBasicApp/ViewController.swift
+++ PracticeBasicApp/PracticeBasicApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLable: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cofigureUI()
     }
-
+    
+    func cofigureUI() {
+        mainLable.text = "choisi le second"
+        slider.setValue(0.5, animated: true)
+    }
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        //슬라이더의 밸류값을 가지고 메인 레이블의 텍스트를 셋팅
+        let seconds = Int(slider.value * 60)
+        mainLable.text = "\(seconds)sec"
+        
+    }
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        //1초씩 지나갈 때마다 실행.
+        mainLable.text = ""
+    }
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        cofigureUI()
+    }
+    
 
 }
 
