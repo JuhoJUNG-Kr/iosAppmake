c679fe84039ebf7c7a1368c6706472f59548bbb9
Juho Jung <jooho293@gmail.com>
2022-07-07 16:08:31 +0200

    Feat: done
    -

--- PracticeBasicApp/PracticeBasicApp/ViewController.swift
+++ PracticeBasicApp/PracticeBasicApp/ViewController.swift
@@ -29,29 +29,47 @@ class ViewController: UIViewController {
     
     @IBAction func sliderChanged(_ sender: UISlider) {
         //슬라이더의 밸류값을 가지고 메인 레이블의 텍스트를 셋팅
-        let seconds = Int(slider.value * 60)
-        mainLable.text = "\(seconds)sec"
-        number = seconds
+        number = Int(sender.value * 60)
+        mainLable.text = "\(number)sec"
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         //1초씩 지나갈 때마다 실행.
-        timer?.invalidate()   // just in case you had existing `Timer`, `invalidate` it before we lose our reference to it
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
-            // do something here 반복하고 싶은 코드
-            if number > 0 {
-                number -= 1
-                slider.value = Float(number) / Float(60)
-                mainLable.text = "\(number) sec"
-            } else {
-                number = 0
-                cofigureUI()
-                timer?.invalidate() //타이머를 사용할 경우 시작하기전, 끝난 후 비활성화 시키기
-                //시스템 사운드 (기본 사운드) 재생
-                AudioServicesPlayAlertSound(SystemSoundID(1322))
-            }
-        }
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(doSomething), userInfo: nil, repeats: true)
+        
+        
+        //클로저를 사용하여 만들기
+//        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
+//            // do something here 반복하고 싶은 코드
+//            if number > 0 {
+//                number -= 1
+//                slider.value = Float(number) / Float(60)
+//                mainLable.text = "\(number) sec"
+//            } else {
+//                number = 0
+//                cofigureUI()
+//                timer?.invalidate() //타이머를 사용할 경우 시작하기전, 끝난 후 비활성화 시키기
+//                //시스템 사운드 (기본 사운드) 재생
+//                AudioServicesPlayAlertSound(SystemSoundID(1322))
+//            }
+//
+//        }
 
+    }
+    //함수를 이용하여 만들기
+    @objc func doSomething() {
+        if number > 0 {
+            number -= 1
+            slider.value = Float(number) / Float(60)
+            mainLable.text = "\(number) sec"
+        } else {
+            number = 0
+            cofigureUI()
+            timer?.invalidate() //타이머를 사용할 경우 시작하기전, 끝난 후 비활성화 시키기
+            //시스템 사운드 (기본 사운드) 재생
+            AudioServicesPlayAlertSound(SystemSoundID(1322))
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
