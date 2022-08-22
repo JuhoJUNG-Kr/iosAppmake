6de473caece352aa83a57aa48cfbbfdd4ca8fd12
Juho Jung <jooho293@gmail.com>
2022-08-22 16:08:17 +0200

    Feat:making viewcon
    -

---
+++ Nav&Tapcodemake/Nav&Tapcodemake/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tapcodemake
+//
+//  Created by 정주호 on 22/08/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
