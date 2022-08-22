d7511a86274cf99cd78e26c91652902475818c54
Juho Jung <jooho293@gmail.com>
2022-08-22 15:42:33 +0200

    Feat: making nav bar & tap bar
    -

---
+++ Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by 정주호 on 22/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
