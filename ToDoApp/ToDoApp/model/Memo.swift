4ba2f54d290c6ccf14ffb6ddd932393dfd154d66
Juho Jung <jooho293@gmail.com>
2022-09-07 16:36:43 +0200

    Start: todo list app
    -

---
+++ ToDoApp/ToDoApp/model/Memo.swift
@@ -0,0 +1,14 @@
+//
+//  Memo.swift
+//  ToDoApp
+//
+//  Created by 정주호 on 07/09/2022.
+//
+
+import Foundation
+
+struct Memo {
+    var memoText: String?
+    var date: Date?
+    var color: Int
+}
